52e6ee78ba6c7d747658a669faf94b14a0080593
david-i-berry <dberry@wmo.int>
2022-10-05 12:38:47 +0200

    upsert debugging

--- toJTWCdat/__init__.py
+++ toJTWCdat/__init__.py
@@ -223,6 +223,7 @@ class BUFR2JTWC(BaseAbstractData):
             LOGGER.debug(f"Item {count}: {item}")
             count += 1
             assert len(upsert_list) > 1
-        upsert_collection_item(self.topic_hierarchy.dotpath, upsert_list)
+        newlist = deepyopc(upsert_list)
+        upsert_collection_item(self.topic_hierarchy.dotpath, newlist)
 
         return True
